2c9bf5718e5cc964bf41a7a3ace24ef97374f8c6
Art <archi_baid@mail.ru>
2026-04-07 14:21:19 +0300

    debug program

--- Calculator.cs
+++ Calculator.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public int Add(int a,int b) => a + b;
+        public int Subtact(int a,int b) => a - b;
+
+    }
+}
